a6d601c1aa5ed7a375c6283df36e75ff490ddb4c
Roberto <robertoj@ciandt.com>
2016-04-28 10:37:54 -0300

    ajustando porta aplicação

--- config/express.js
+++ config/express.js
@@ -16,7 +16,7 @@ module.exports = function() {
 	app.use(require('method-override')());
 
 	// Condig. de Porta
-	app.set('port', 3000);
+	app.set('port', 80);
 
 	// Config. conteúdo público
 	app.use(express.static('../public'));
