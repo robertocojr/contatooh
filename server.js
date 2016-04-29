f0b2bd9f3d5bdbff637a49d7649c126982887f60
Roberto <robertoj@ciandt.com>
2016-04-29 10:00:18 -0300

    configurando moongo mLab

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var express = require('./config/express');
 var app = express();
 
 require('./config/passport')();
-require('./config/database')('mongodb://localhost/contatooh');
+require('./config/database')('mongodb://contatooh:pwcontatooh@ds063134.mlab.com:63134/rcandidodb');
+// require('./config/database')('mongodb://localhost/contatooh');
 
 const port = app.get('port')
 
