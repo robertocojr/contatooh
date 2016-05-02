9c7d44cd9eaba045945eb1928cce105197a54f65
Roberto Júnior <robertoco.jr@gmail.com>
2016-05-01 21:55:48 -0300

    Implementação de testes na camada web

---
+++ config/protractor.js
@@ -0,0 +1,10 @@
+exports.config = {
+  specs: ['../test/e2e/**/*.js'],
+  onPrepare: function() {
+    browser.driver.get('http://localhost:3000');
+    browser.driver.findElement(by.id('entrar')).click();
+    browser.driver.findElement(by.id('login_field')).sendKeys('usuario');
+    browser.driver.findElement(by.id('password')).sendKeys('senha');
+    browser.driver.findElement(by.name('commit')).click();
+  }
+};
